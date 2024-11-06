70190f389f48c70f0ba6920c6853af39624ea325
minhhkhoa <khoanguyen.1881@gmail.com>
2024-11-06 13:46:26 +0700

    tich hop graphql co ban

--- index.ts
+++ index.ts
@@ -1,26 +1,48 @@
-import express, {Express, Request, Response} from "express"
+import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
 import * as database from "./config/database"
-import Article from "./model/article.model"
+import { ApolloServer, gql } from "apollo-server-express"
 
-dotenv.config()
+const startServer = async () => {
+  dotenv.config()
 
-database.connect()
+  database.connect()
 
-const app: Express = express()
-const port: number | string = process.env.PORT || 3000
+  const app: Express = express()
+  const port: number | string = process.env.PORT || 3000
 
-//rest API
-app.get("/articles", async (req: Request, res: Response) => {
+  //-graphQL
+  //-tao kieu tra ve cua ham 
+  const typeDefs = gql` 
+    type Query {
+      hello: String
+    }
+  `
 
-  const articles = await Article.find({
-    deleted: false
+  const resolvers = { //-viet ham vs kieu tra ve ben tren
+    Query: {
+      hello: () => {
+        return "hello world"
+      }
+    }
+  }
+
+  const apolloServer = new ApolloServer({ //- tao ra server
+    typeDefs,
+    resolvers
   })
-  res.json({
-    articles: articles
-  })
-})
 
-app.listen(port, () => {
-  console.log(`App listenning on port ${port}`)
-})
+  await apolloServer.start()
+
+  //tao api toi graphql
+  apolloServer.applyMiddleware({
+    app: app,
+    path: "/graphql"
+  })
+
+  app.listen(port, () => {
+    console.log(`App listenning on port ${port}`)
+  })
+}
+
+startServer()
